1de5dcb4744f1c7c6d0bc644a07ea626b379b936
codenickstar <codenickstar@fastmail.com>
2018-11-06 02:47:23 +0100

    Implement parsing for first page

--- album/album.go
+++ album/album.go
@@ -2,10 +2,15 @@ package album
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"go.uber.org/zap"
+
+	"golang.org/x/net/html"
 )
 
 // Album represents one image album
@@ -16,10 +21,23 @@ type Album struct {
 	name         string
 	downloadPath string
 
-	rawPictureURLs []string
-	pictureURLs    []*url.URL
+	pagesCount int
+	pages      []Page
 
 	logger *zap.SugaredLogger
+}
+
+const nextPageID = "next_url"
+
+// Page represents one page
+type Page struct {
+	nextPageURL string
+
+	thumbnailSrc   string
+	thumbnailID    string
+	dynamicIndices [3]int
+
+	pictureURL string
 }
 
 // New returns a new instance of Album
@@ -27,7 +45,7 @@ func New(logger *zap.SugaredLogger, rawURL string) (a Album, err error) {
 	a.logger = logger
 	a.url, err = url.Parse(rawURL)
 	a.user = strings.Split(a.url.Path, "/")[1]
-	a.logger.Debugw("created new album",
+	a.logger.Infow("created album",
 		"url", a.url, "user", a.user,
 	)
 	return
@@ -35,8 +53,110 @@ func New(logger *zap.SugaredLogger, rawURL string) (a Album, err error) {
 
 // Download downloads the album
 func (a *Album) Download() (err error) {
-	// fetch the source
+	a.logger.Info("downloading the album", a)
+
+	// fetch the source for the first page
+	resp, err := http.Get(a.url.String())
+	if err != nil {
+		a.logger.DPanic("error fetching the source: ", err)
+	}
+
 	// parse the source
+	page := Page{}
+	tokenizer := html.NewTokenizer(resp.Body)
+	endOfDocument := false
+	for !endOfDocument {
+		tokenType := tokenizer.Next()
+		switch {
+		case tokenType == html.ErrorToken:
+			// end of document
+			endOfDocument = true
+			break
+		case tokenType == html.TextToken:
+			token := tokenizer.Token()
+			// parse the title
+			if strings.Contains(token.String(), "@iMGSRC.RU") {
+				a.name = token.String()[:strings.LastIndex(token.String(), ",")]
+				a.logger.Debug("parsed album name: ", a.name)
+				continue
+			}
+			// parse the indices
+			// i=q.slice(z+1,60)+e[7]+e.charAt(6)+e[2];
+			const indiceRegex = `(?mU)^i=q\.slice\(z\+1,60\)\+.+(?:\[|\()(\d)(?:\]|\)).+(?:\[|\()(\d)(?:\]|\)).+(?:\[|\()(\d)(?:\]|\));$`
+			iLiteral := "i=q.slice(z+1,60)+"
+			if strings.Contains(token.String(), iLiteral) {
+				r := regexp.MustCompile(indiceRegex)
+				matches := r.FindAllStringSubmatch(token.String(), 3)
+				a.logger.Debug(matches)
+				// loop through the submatches and assign them
+				for i := 0; i < 3; i++ {
+					page.dynamicIndices[i], err = strconv.Atoi(matches[0][i+1])
+					if err != nil {
+						a.logger.Error(err)
+					}
+				}
+			}
+			break
+		case tokenType == html.StartTagToken:
+			token := tokenizer.Token()
+
+			// check if current token is img or a tag
+			if !((token.Data == "img") || (token.Data == "a")) {
+				continue
+			}
+
+			// loop through all attributes
+			for outerAttKey, outerAtt := range token.Attr {
+				// get the thumbnail and the next page
+				for _, innerAtt := range token.Attr {
+					if (outerAtt.Key == "src" && innerAtt.Key == "id") || (outerAtt.Key == "id" && outerAtt.Val == nextPageID) {
+						// print all the token
+						a.logger.Debug(token)
+
+						// next url
+						if innerAtt.Key == "href" {
+							page.nextPageURL = innerAtt.Val
+						}
+
+						// thumbnail
+						if len(innerAtt.Val) == 8 && (innerAtt.Val != nextPageID) {
+							page.thumbnailID = innerAtt.Val
+							page.thumbnailSrc = token.Attr[outerAttKey].Val
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// do the magic
+	err = page.FormatPictureLink()
+	if err != nil {
+		a.logger.DPanic(err)
+		return
+	}
+	a.logger.Info(page)
+
+	return
+}
+
+func ParsePage() {
+
+}
+
+// FormatPictureLink formats the picture link from all the attributes
+func (p *Page) FormatPictureLink() (err error) {
+	lastIndexOfSlash := strings.LastIndex(p.thumbnailSrc, "/")
+
+	p.pictureURL = fmt.Sprintf(
+		"https://b%s/%s%s%s%s.jpg",
+		p.thumbnailSrc[9:lastIndexOfSlash],
+		p.thumbnailSrc[lastIndexOfSlash+1:60],
+		string(p.thumbnailID[p.dynamicIndices[0]]),
+		string(p.thumbnailID[p.dynamicIndices[1]]),
+		string(p.thumbnailID[p.dynamicIndices[2]]),
+	)
+
 	return
 }
 
--- main.go
+++ main.go
@@ -3,37 +3,50 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"imgdl/album"
 	"os"
+
+	"github.com/codenickstar/imgdl/album"
 
 	"go.uber.org/zap"
 )
 
 func main() {
+	// logger
 	logger, _ := zap.NewDevelopment()
 	defer logger.Sync()
 	l := logger.Sugar()
-	var job album.Album
 
-	// read album url from stdin
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Enter album URL:")
-	for scanner.Scan() {
-		line := scanner.Text()
-		if len(line) != 0 {
-			var err error
-			job, err = album.New(l, line)
-			if err != nil {
-				continue
+	// album
+	var job album.Album
+	var err error
+
+	// read album from env
+	url := os.Getenv("ALBUM_URL")
+	if url == "" {
+		// read album url from stdin
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Println("Enter album URL:")
+		for scanner.Scan() {
+			line := scanner.Text()
+			if len(line) != 0 {
+				job, err = album.New(l, line)
+				if err != nil {
+					continue
+				}
+				break
 			}
-			break
+			fmt.Println("Please enter a valid URL:")
 		}
-		fmt.Println("Please enter a valid URL:")
+	} else {
+		job, err = album.New(l, url)
+		if err != nil {
+			l.DPanic("error creating album", err)
+		}
 	}
 
 	// start new job
-	err := job.Download()
+	err = job.Download()
 	if err != nil {
-		panic(err)
+		l.DPanic(err)
 	}
 }
